f864de79b61688b2d72080a03f526a48fb4cd84f
Kirill Grouchnikov <kirill.grouchnikov@gmail.com>
2024-06-25 15:06:16 -0400

    Modernize the transcoder sample project
    * Migrate to gradle.kts (settings and build)
    * Add versions TOML catalog
    * Configure the generated classes to be under src/gen
    * Upgrade to Radiance 7.5.0

---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    java
+    idea
+}
+
+group = "org.pushing-pixels.radiance.demo"
+version = "1.0.0"
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_11
+
+buildscript {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven("https://plugins.gradle.org/m2/")
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+
+    dependencies {
+        classpath(libs.radiance.gradlePlugin)
+        classpath(libs.versionchecker.gradlePlugin)
+    }
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+}
+
+dependencies {
+    implementation(libs.radiance.common)
+}
+
+tasks.register<org.pushingpixels.radiance.tools.svgtranscoder.gradle.TranscodeDeepTask>("transcodeDeep") {
+    inputRootDirectory = file("src/main/resources")
+    outputRootDirectory = file("src/gen/java/org/radiance/demo/svg")
+    outputRootPackageName = "org.radiance.demo.svg"
+    outputLanguage = "java"
+    transcode()
+}
+
+tasks.withType<JavaCompile> {
+    dependsOn("transcodeDeep")
+}
+
+java {
+    sourceSets {
+        java {
+            sourceSets["main"].apply {
+                java.srcDir("$rootDir/src/main/java")
+                java.srcDir("$rootDir/src/gen/java")
+            }
+        }
+    }
+}
+
+idea {
+    module {
+        generatedSourceDirs.add(file("$rootDir/src/gen/kotlin"))
+    }
+}
+
+// To generate report about available dependency updates, run
+// ./gradlew dependencyUpdates
+apply(plugin = "com.github.ben-manes.versions")
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+
+}
+rootProject.name = "radiance-svg-transcoder-plugin-test"
+
